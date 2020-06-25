f5900cc335583d090bccee87f4558c57bd2a7c23
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-06-25 10:49:08 -0400

    changes

---
+++ main.js
@@ -0,0 +1,17 @@
+function challengeBanner(n){
+    console.log('###### Challenge ${n} ######')
+}
+
+function onlyVowels(str){
+    let i = 0;
+    while (i< str.length){
+    //    if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i')
+   // }
+
+if (onlyVowels.includes(str[i])){
+    console.log(str[i]);
+}
+
+i= i + 1;
+}
+}
